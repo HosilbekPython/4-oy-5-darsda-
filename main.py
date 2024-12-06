e61266e9fbb8d10f5f203f1a310fe7f2f6aed9df
Hosilbek <hosilbektohtarov45@gmail.com>
2024-12-07 00:08:00 +0500

    Datetime Moduli_Py2

--- main.py
+++ main.py
@@ -1,72 +1,83 @@
 """
-Descriptorlar
+Datetime Moduli
 """
-from bisect import insort
 
+# 1 vazifa
 
-1 vazifa
-
-class Deckriptor:
-    def __set_name__(self, owner, name):
-        self.name = name
-
-
-    def __get__(self, instance, owner):
-        return instance.__dict__.get(self.name , None)
-
-    def __set__(self, instance, value):
-        if not isinstance(value, str):
-            raise ValueError("Qiymat faqat matn bo'lishi kerak!")
-        if value.isdigit():
-            print(f"{value} bu harfli matn emas!")
-        else:
-            instance.__dict__[self.name] = value.lower()
-
-class Tekshirish:
-    name = Deckriptor()
-    def __init__(self , name):
-        self.name = name
-
-
-text = input("Soz kirirting : ")
-p1 = Tekshirish(text)
-print(p1.name)
-
-
-# -------------------------------------------------------------------
+#
+# from datetime import date ,datetime , timedelta
+#
+#
+# class Datani_olish:
+#     def bugungi_sana(self):
+#         bugungi_sana = date.today()
+#         return bugungi_sana
+#
+#     def yetti_kun_oldin(self):
+#         yeti = timedelta(days=7)
+#         bugungi_sana = date.today()
+#         return bugungi_sana - yeti
+#
+#     def yetti_kun_keyin(self):
+#         yeti = timedelta(days=7)
+#         bugungi_sana = date.today()
+#         return bugungi_sana + yeti
+#
+# sana = Datani_olish()
+# print(sana.bugungi_sana())
+# print(sana.yetti_kun_oldin())
+# print(sana.yetti_kun_keyin())
 
 
 
 # 2 vazifa
 
 
-class RaqamgaTekshirish:
+# from datetime import date ,datetime , timedelta
+#
+# class Tugulgan_kun_qoldi:
+#     def __init__(self , sana_str):
+#         self.sana_str = sana_str
+#
+#     def sana_string_to_date(self):
+#         sana = datetime.strptime(self.sana_str , "%d-%m-%Y")
+#         bugun = datetime.now()
+#         if sana.replace(year=bugun.year) < bugun:
+#             sana = sana.replace(year=bugun.year + 1)
+#
+#         else:
+#             sana = sana.replace(year=bugun.year)
+#
+#         qoldi = sana - bugun
+#         if qoldi.days == 364:
+#             return "Tugulgan kunigiz bilan 🔥🔥 "
+#         else:
+#             return f"{qoldi.days} kun "
+# tugulgan_kun = input("Tugulgan kunigizni yuboring (dd-mm-YYYY) : ")
+# kun1 = Tugulgan_kun_qoldi(tugulgan_kun)
+# print(kun1.sana_string_to_date())
 
-    def __set_name__(self, owner, name):
-        self.name = name
 
-    def __get__(self, instance, owner):
-        return instance.__dict__[self.name]
 
-    def __set__(self, instance, value:str):
 
-        if value.isalpha():
-            raise ValueError("Qiymat faqat raqam bo'lishi kerak!")
-        else:
-            instance.__dict__[self.name] = value
 
-    def __delattr__(self, instance):
-        del instance.__dict__[self.name]
+# 3 vazifa
 
-class Tekshirish:
-    raqam = RaqamgaTekshirish()
-
-    def __init__(self , raqam):
-        self.raqam  = raqam
-
-raqam = input("Raqam kriting : ")
-r1 = Tekshirish(raqam)
-print(r1.raqam)
+# import time
+#
+#
+# time1 = "12:10:00"
+# time2 = "12:10:50"
+#
+# timee1 = time.strptime(time1 , "%H:%M:%S")
+# timee2 = time.strptime(time2 , "%H:%M:%S")
+#
+# time1_sec = timee1.tm_hour * 3600 + timee1.tm_min * 60 + timee1.tm_sec
+# time2_sec = timee2.tm_hour * 3600 + timee2.tm_min * 60 + timee2.tm_sec
+#
+# farqi = time2_sec - time1_sec
+#
+# print(farqi)
 
 
 
